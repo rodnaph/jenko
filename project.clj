e7b19a6d51334c6543e6384252b15acf1ca9ff2b
Rhodri Pugh <rod@pu-gh.com>
2012-12-18 18:17:28 +0000

    updated project details

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 
-(defproject jenko "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject jenko "0.1.0"
+  :description "Clojure helpers for Jenkins"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.5.8"]
                  [cheshire "4.0.4"]
                  [enlive "1.0.1"]])
+
